d1f7b990ff06b6f876e393b027b8918416c4ef51
simon.zhang <simon.zhang@kilimall.com>
2017-05-22 14:04:17 +0800

    更改模型

--- test.php
+++ test.php
@@ -1,3 +1,7 @@
 <?php
   echo htmlspecialchars_decode('/index.php?m=Home&amp;c=Index&amp;a=index')
+
+  a:10:{s:13:"payment_price";s:0:"";s:15:"nopayment_price";s:0:"";s:9:"receiptno";s:4:"D148";s:7:"cashier";s:9:"727800700";s:12:"sales_person";s:9:"727800700";s:11:"optometrist";s:9:"727800700";s:14:"payment_method";s:11:"insurance_1";s:13:"insurancetext";s:7:"1111111";s:8:"add_time";i:1495378865;s:18:"total_order_amount";d:5000;}
+
+
   ?>
