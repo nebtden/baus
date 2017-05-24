a7a673b0c6c6dd6137883777abef8c89be91f73f
simon.zhang <simon.zhang@kilimall.com>
2017-05-25 00:04:28 +0800

    修复bug

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -363,35 +363,6 @@ class IndexController extends Controller
 
 
 
-        /* ====== Step5: Insurance Proceed Customer Insurance Card Report Section ====== */
-        if ($step_name == 'edit_customer_card_proceed') {
-            unset($update_data);
-            unset($data);
-
-            $data['customer_card_confirm'] = I('post.customer_card_confirm');
-            $data['add_time'] = time();
-
-            $corporate = unserialize($order_info['corporate']);
-
-            /* Might be no need for this step ----------------------------------------------------*/
-            if ($corporate['corporate_status_set'] == 'proceed') {
-                $update_data['order_step'] = 7;
-                $update_data['balance'] = 0;
-            } else {
-                $update_data['order_step'] = 6; //Fail Safe solution
-            }
-            /* Might be no need for this step ----------------------------------------------------*/
-
-            $update_data['customer_card'] = serialize($data);
-
-            M('order_info')->where(['order_id' => $order_id])->save($update_data);
-
-            if (M('order_info')->where(['order_id' => $order_id])->save($update_data) !== FALSE) {
-                $this->success('save success', U('Index/orderShow', array('order_id' => $order_id)));
-            } else {
-                $this->error('save fail', U('Index/orderShow', array('order_id' => $order_id)));
-            }
-        }
         /* ====== Step5 Customer Insurance Card Report Section END ====== */
 
         /* ====== Step 5 (If corporate manager chooses less approval even with NO SMART CARD) ====== */
@@ -399,84 +370,7 @@ class IndexController extends Controller
             unset($update_data);
             unset($data);
 
-            if (I('post.less_approve_action') == 'cancel') {
-                $update_data['order_step'] = -1;
 
-            } else {
-
-                //Change goods for now is just to have a balance of 0
-                if (I('post.less_approve_action') == 'top_up') {
-                    $data['balance_remaining'] = I('post.balance_remaining');
-                    $update_data['balance'] = I('post.balance_remaining');
-                    $update_data['topup'] = 1;
-
-                } elseif (I('post.less_approve_action') == 'change_goods') {
-                    $data['balance_remaining'] = 0;
-                    $update_data['balance'] = 0;
-                    $update_data['topup'] = 0;
-                } elseif (I('post.less_approve_action') == 'discount'){
-                    $data['balance_remaining'] = 0;
-                    $update_data['balance'] = 0;
-                    $update_data['topup'] = 1;
-                }
-
-                //if smart card or not
-                $receiptno = unserialize($order_info['receiptno']);
-
-                if ($receiptno['payment_method'] == 'insurance_0') { // NO SMART CARD
-
-                    $update_data['order_step'] = I('post.balance_remaining') == 0 ? 7 : 5;
-
-                } elseif ($receiptno['payment_method'] == 'insurance_1') { // WITH SMART CARD
-
-                    $update_data['order_step'] = I('post.balance_remaining') == 0 ? 6 : 5;
-                }
-
-
-                $data['less_approve_action'] = I('post.less_approve_action');
-
-
-                $data['remark'] = I('post.remark');
-
-
-                $data['less_approve_pay_method'] = 'combine';
-
-
-                //收取买家费用
-                $payed_money = insertpaidmoney($order_info,'');
-                $data['recevied_amount'] = $payed_money;
-                //更新余额
-                M('order_info')->where(['order_step'=>7,'order_id'=>$order_id])->save([
-                    'balance' => 0
-                ]);
-
-//                $data_cash['order_id'] = $order_info['order_id'];
-//                $data_cash['order_sn'] = $order_info['order_sn'];
-//                $data_cash['pay_amount'] = I('post.recevied_amount');
-//                $data_cash['payment_method'] = I('post.less_approve_pay_method');
-//                $data_cash['receipt_no'] = 'B' . $cash_count;
-//                $data_cash['payment_remark'] = I('post.remark');
-//                $data_cash['reverse_status'] = 0;
-//                $data_cash['cancel_status'] = 0;
-//                $data_cash['member_id'] = session('member_id');
-//                $data_cash['add_time'] = time();
-//
-//                M('cash')->add($data_cash);
-
-                $data['add_time'] = time();
-
-                $update_data['customer_confirm'] = serialize($data);
-
-
-                M('order_info')->where(['order_id' => $order_id])->save($update_data); //update customer_confirm
-
-            }
-
-            if (M('order_info')->where(['order_id' => $order_id])->save($update_data) !== FALSE) {
-                $this->success('save success', U('Index/orderShow', array('order_id' => $order_id)));
-            } else {
-                $this->error('save fail', U('Index/orderShow', array('order_id' => $order_id)));
-            }
 
 
         }
--- Application/Home/Controller/OrderController.class.php
+++ Application/Home/Controller/OrderController.class.php
@@ -52,6 +52,8 @@ class OrderController extends Controller
         $this->display();
     }
 
+
+
     public function editcorporate(){
         /* ====== Step 4 Corporate Manager Section ====== */
 
@@ -99,6 +101,149 @@ class OrderController extends Controller
         /* ====== Step 4, Corporate Manager Section END ====== */
 
     }
+
+    public function confirm(){
+        $order_id = I('get.order_id');
+        $order_info = M('order_info')->where(['order_id' => $order_id])->find();
+        $receiptno = unserialize($order_info['receiptno']);
+        $customer_confirm = unserialize($order_info['customer_confirm']);
+
+        $this->assign('order_info', $order_info);
+        $this->assign('receiptno', $receiptno);
+        $this->assign('customer_confirm', $customer_confirm);
+
+        $this->display();
+    }
+
+
+    public function confirmsave(){
+        $order_id = I('post.order_id');
+        $order_info = M('order_info')->where(['order_id' => $order_id])->find();
+
+        if (I('post.less_approve_action') == 'cancel') {
+            $update_data['order_step'] = -1;
+
+        } else {
+
+            //Change goods for now is just to have a balance of 0
+            if (I('post.less_approve_action') == 'top_up') {
+                $data['balance_remaining'] = I('post.balance_remaining');
+                $update_data['balance'] = I('post.balance_remaining');
+                $update_data['topup'] = 1;
+
+            } elseif (I('post.less_approve_action') == 'change_goods') {
+                $data['balance_remaining'] = 0;
+                $update_data['balance'] = 0;
+                $update_data['topup'] = 0;
+            } elseif (I('post.less_approve_action') == 'discount'){
+                $data['balance_remaining'] = 0;
+                $update_data['balance'] = 0;
+                $update_data['topup'] = 1;
+            }
+
+            //if smart card or not
+            $receiptno = unserialize($order_info['receiptno']);
+
+            if ($receiptno['payment_method'] == 'insurance_0') { // NO SMART CARD
+
+                $update_data['order_step'] = I('post.balance_remaining') == 0 ? 7 : 5;
+
+            } elseif ($receiptno['payment_method'] == 'insurance_1') { // WITH SMART CARD
+
+                $update_data['order_step'] = I('post.balance_remaining') == 0 ? 6 : 5;
+            }
+
+
+            $data['less_approve_action'] = I('post.less_approve_action');
+
+
+            $data['remark'] = I('post.remark');
+
+
+            $data['less_approve_pay_method'] = 'combine';
+
+
+            //收取买家费用
+            $payed_money = insertpaidmoney($order_info,'');
+            $data['recevied_amount'] = $payed_money;
+            //更新余额
+            M('order_info')->where(['order_step'=>7,'order_id'=>$order_id])->save([
+                'balance' => 0
+            ]);
+
+//                $data_cash['order_id'] = $order_info['order_id'];
+//                $data_cash['order_sn'] = $order_info['order_sn'];
+//                $data_cash['pay_amount'] = I('post.recevied_amount');
+//                $data_cash['payment_method'] = I('post.less_approve_pay_method');
+//                $data_cash['receipt_no'] = 'B' . $cash_count;
+//                $data_cash['payment_remark'] = I('post.remark');
+//                $data_cash['reverse_status'] = 0;
+//                $data_cash['cancel_status'] = 0;
+//                $data_cash['member_id'] = session('member_id');
+//                $data_cash['add_time'] = time();
+//
+//                M('cash')->add($data_cash);
+
+            $data['add_time'] = time();
+
+            $update_data['customer_confirm'] = serialize($data);
+
+
+            M('order_info')->where(['order_id' => $order_id])->save($update_data); //update customer_confirm
+
+        }
+
+        if (M('order_info')->where(['order_id' => $order_id])->save($update_data) !== FALSE) {
+            $this->success('save success', U('Index/Index', array('order_step' => 6)));
+        } else {
+            $this->error('save fail', U('Order/confirm', array('order_id' => $order_id)));
+        }
+
+    }
+
+
+    public function cardconfirm(){
+        $order_id = I('get.order_id');
+        $order_info = M('order_info')->where(['order_id' => $order_id])->find();
+        $receiptno = unserialize($order_info['receiptno']);
+
+        $this->assign('order_info', $order_info);
+        $this->assign('receiptno', $receiptno);
+
+        $this->display();
+    }
+
+    public function cardconfirmsave(){
+        $order_id = I('post.order_id');
+        $order_info = M('order_info')->where(['order_id' => $order_id])->find();
+
+        $data['customer_card_confirm'] = I('post.customer_card_confirm');
+        $data['add_time'] = time();
+
+        $corporate = unserialize($order_info['corporate']);
+
+        /* Might be no need for this step ----------------------------------------------------*/
+        if ($corporate['corporate_status_set'] == 'proceed') {
+            $update_data['order_step'] = 7;
+            $update_data['balance'] = 0;
+        } else {
+            $update_data['order_step'] = 6; //Fail Safe solution
+        }
+        /* Might be no need for this step ----------------------------------------------------*/
+
+        $update_data['customer_card'] = serialize($data);
+
+        M('order_info')->where(['order_id' => $order_id])->save($update_data);
+
+        if (M('order_info')->where(['order_id' => $order_id])->save($update_data) !== FALSE) {
+            $this->success('save success', U('Index/Index', array('order_step' => 5)));
+        } else {
+            $this->error('save fail', U('Order/cardconfirm', array('order_id' => $order_id)));
+        }
+
+    }
+
+
 
 
     public function makeOrder()
